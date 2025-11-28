0328c417829c1b205e2c7ee5cac3e49b4e73ea48
MadDogOwner <xiaoran@xrgzs.top>
2025-11-28 11:39:18 +0800

    refactor(pdf.js): rename mjs to js
    Signed-off-by: MadDogOwner <xiaoran@xrgzs.top>

--- pdf.js/build/pdf.js
+++ pdf.js/build/pdf.js
@@ -21471,7 +21471,7 @@ class PDFWorker {
   static {
     if (isNodeJS) {
       this.#isWorkerDisabled = true;
-      GlobalWorkerOptions.workerSrc ||= "./pdf.worker.mjs";
+      GlobalWorkerOptions.workerSrc ||= "./pdf.worker.js";
     }
     this._isSameOrigin = (baseUrl, otherUrl) => {
       const base = URL.parse(baseUrl);
